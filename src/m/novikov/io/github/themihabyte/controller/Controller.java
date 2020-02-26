ec84ed444b3adf77ada7c428d0fbbabce0d433fb
themihabyte <mihabayt@gmail.com>
2020-02-26 20:22:48 +0200

    New methods: show excellent second-year students, show foreign well-studying students Added comments

--- src/m/novikov/io/github/themihabyte/controller/Controller.java
+++ src/m/novikov/io/github/themihabyte/controller/Controller.java
@@ -6,23 +6,28 @@ import m.novikov.io.github.themihabyte.view.View;
 public class Controller {
     Service service;
     View view;
+    int n = 10; // Number of students
 
+    // Start program with this method
     public void run(){
-        boolean run = true;
-        service.setStudents(10);
+        boolean run = true; // When false - exit from program
+
+        service.setStudents(n); // Get n students from some data source
+
         do {
+            // Menu cycle
             switch (view.menu()) {
                 case 1:
-                    showAllStudentsList();
+                    showAllStudentsList(); // Display all students from source
                     break;
                 case 2:
-                    // TODO show excellent second-year students
+                    showExcellentSecondYearStudents();
                     break;
                 case 3:
-                    // TODO show foreign well-studying students
+                    showForeignWellStudyingStudents();
                     break;
                 case 4:
-                    run = false;
+                    run = false; // Exit from program
                     break;
             }
         } while (run);
@@ -33,7 +38,18 @@ public class Controller {
         this.view = new View();
     }
 
+    // Displays all students on screen
     private void showAllStudentsList(){
         view.students(service.getStudents());
+    }
+
+    // Displays only excellent second-year students
+    private void showExcellentSecondYearStudents(){
+        view.students(service.getExcellentSecondYearStudents());
+    }
+
+    // Displays only foreign student whose mark > 85
+    private void showForeignWellStudyingStudents(){
+        view.students(service.getForeignWellStudyingStudents());
     }
 }
