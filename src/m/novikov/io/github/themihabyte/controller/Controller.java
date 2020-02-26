d0d7ab3ac67be885abb5aaea8226adb1b20f76c5
themihabyte <mihabayt@gmail.com>
2020-02-26 16:04:03 +0200

    Shows list of students Created run method Added view and service as fields Created constructor

--- src/m/novikov/io/github/themihabyte/controller/Controller.java
+++ src/m/novikov/io/github/themihabyte/controller/Controller.java
@@ -1,7 +1,39 @@
 package m.novikov.io.github.themihabyte.controller;
 
-public class Controller {
-    public void run(){
+import m.novikov.io.github.themihabyte.model.Service;
+import m.novikov.io.github.themihabyte.view.View;
 
+public class Controller {
+    Service service;
+    View view;
+
+    public void run(){
+        boolean run = true;
+        service.setStudents(10);
+        do {
+            switch (view.menu()) {
+                case 1:
+                    showAllStudentsList();
+                    break;
+                case 2:
+                    // TODO show excellent second-year students
+                    break;
+                case 3:
+                    // TODO show foreign well-studying students
+                    break;
+                case 4:
+                    run = false;
+                    break;
+            }
+        } while (run);
+    }
+
+    public Controller() {
+        this.service = new Service();
+        this.view = new View();
+    }
+
+    private void showAllStudentsList(){
+        view.students(service.getStudents());
     }
 }
