076615dbf5ec4de0d6cf3a924637a04458badc81
themihabyte <mihabayt@gmail.com>
2020-02-26 16:07:22 +0200

    Gets key from keyboard New field Scanner (initializing in constructor)

--- src/m/novikov/io/github/themihabyte/view/Request.java
+++ src/m/novikov/io/github/themihabyte/view/Request.java
@@ -1,4 +1,17 @@
 package m.novikov.io.github.themihabyte.view;
 
+import java.util.Scanner;
+
 public class Request {
+    Scanner scanner;
+
+    int getMenuKey() {
+        int key=scanner.nextInt();
+        if (Validator.validateMenuKey(key)) return key;
+        return -1;
+    }
+
+    Request() {
+        this.scanner=new Scanner(System.in);
+    }
 }
