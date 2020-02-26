dcf7f9b62811f1be9c43c982a7dc63d4edaa5abb
themihabyte <mihabayt@gmail.com>
2020-02-26 16:10:14 +0200

    validate menu key is done

--- src/m/novikov/io/github/themihabyte/view/Validator.java
+++ src/m/novikov/io/github/themihabyte/view/Validator.java
@@ -1,4 +1,11 @@
 package m.novikov.io.github.themihabyte.view;
 
 public class Validator {
+    static boolean validateMenuKey(int key) {
+        if (key > 4 || key < 1) {
+            System.out.println("Input Another key");
+            return false;
+        }
+        return true;
+    }
 }
