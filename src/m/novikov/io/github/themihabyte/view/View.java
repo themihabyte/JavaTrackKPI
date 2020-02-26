96c9be72ecba1ea12ca7577feb8f4abc01ab4426
themihabyte <mihabayt@gmail.com>
2020-02-26 16:12:18 +0200

    connects Request and Response

--- src/m/novikov/io/github/themihabyte/view/View.java
+++ src/m/novikov/io/github/themihabyte/view/View.java
@@ -1,4 +1,26 @@
 package m.novikov.io.github.themihabyte.view;
 
+import m.novikov.io.github.themihabyte.model.Student;
+
 public class View {
+    Request request;
+    Response response;
+
+    public View(){
+        this.request = new Request();
+        this.response = new Response();
+    }
+
+    public int menu(){
+        int key;
+        do {
+            response.displayMenuItems();
+            key = request.getMenuKey();
+        } while (key == -1);
+        return key;
+    }
+
+    public void students(Student[] students){
+        response.displayStudents(students);
+    }
 }
