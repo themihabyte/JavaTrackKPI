10458570c61d7253578690126317699e3c3d52c9
themihabyte <mihabayt@gmail.com>
2020-02-26 16:05:06 +0200

    All methods became static Changed generator parameters

--- src/m/novikov/io/github/themihabyte/model/DataSource.java
+++ src/m/novikov/io/github/themihabyte/model/DataSource.java
@@ -3,31 +3,35 @@ package m.novikov.io.github.themihabyte.model;
 import java.util.Random;
 
 public class DataSource {
-    Student generateStudent() {
+    private static Random random=new Random();
+
+    static Student generateStudent() {
         Student student=new Student();
 
-        student.setName(generateString((generateRandomNumber() % 8)+4)); // length of name = [4;11]
-        student.setSurname(generateString((generateRandomNumber() % 12)+5)); // Length of surname = [5;16]
-        student.setLastName(generateString((generateRandomNumber() % 12)+5)); // Length of last name = [5;16]
-        student.setCountry(generateString((generateRandomNumber() % 15)+5)); // length of country = [5; 19]
-        student.setStudentID(generateRandomNumber() % 1000); // in range: [0;999]
-        student.setGender(new Random().nextBoolean());
-        student.setMark((byte) (generateRandomNumber() % 101)); // mark in range: [0;100]
-        student.setStudyYear((byte) (generateRandomNumber() % 6+1)); // study year: [1 ; 6]
+        student.setName(generateString(generateRandomNumber(4, 11))); // length of name = [4;11]
+        student.setSurname(generateString(generateRandomNumber(5, 15))); // Length of surname = [5;16]
+        student.setLastName(generateString(generateRandomNumber(5, 15))); // Length of last name = [5;16]
+        student.setStudentID(generateRandomNumber(0, 1000)); // in range: [0;999]
+        student.setGender(random.nextBoolean());
+        student.setMark((byte) (generateRandomNumber(0, 100))); // mark in range: [0;100]
+        student.setStudyYear((byte) (generateRandomNumber(1, 6))); // study year: [1 ; 6]
+
+        // Generating random country
+        String[] countries={"Ukraine", "Germany", "England", "Russia", "China", "Netherlands"};
+        student.setCountry(countries[(generateRandomNumber(0, 5))]);
 
         return student;
     }
 
-    private int generateRandomNumber() {
-        Random random=new Random();
-        return random.nextInt();
+    private static int generateRandomNumber(int min, int max) {
+        return random.nextInt((max - min) + 1) + min;
     }
 
     // Generates random string with given length
-    private String generateString(int length) {
+    private static String generateString(int length) {
         StringBuilder str=new StringBuilder();
         for (int i=0; i < length; i++) {
-            str.append((char) (generateRandomNumber() % 25+65));
+            str.append((char) (generateRandomNumber(65, 90)));
         }
         return str.toString();
     }
