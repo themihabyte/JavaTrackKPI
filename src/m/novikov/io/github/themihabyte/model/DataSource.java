311fa114c1cf7f215ebbde7d3c348203ec7fa254
themihabyte <mihabayt@gmail.com>
2020-02-26 20:23:43 +0200

    Improved country generation Added comments

--- src/m/novikov/io/github/themihabyte/model/DataSource.java
+++ src/m/novikov/io/github/themihabyte/model/DataSource.java
@@ -8,22 +8,23 @@ public class DataSource {
     static Student generateStudent() {
         Student student=new Student();
 
-        student.setName(generateString(generateRandomNumber(4, 11))); // length of name = [4;11]
-        student.setSurname(generateString(generateRandomNumber(5, 15))); // Length of surname = [5;16]
-        student.setLastName(generateString(generateRandomNumber(5, 15))); // Length of last name = [5;16]
-        student.setStudentID(generateRandomNumber(0, 1000)); // in range: [0;999]
-        student.setGender(random.nextBoolean());
-        student.setMark((byte) (generateRandomNumber(0, 100))); // mark in range: [0;100]
-        student.setStudyYear((byte) (generateRandomNumber(1, 6))); // study year: [1 ; 6]
+        student.setName(generateString(generateRandomNumber(4, 11))); // length of name generates in range: [4;11]
+        student.setSurname(generateString(generateRandomNumber(5, 15))); // Length of surname generates in range [5;15]
+        student.setLastName(generateString(generateRandomNumber(5, 15))); // Length of last name generates in range [5;15]
+        student.setStudentID(generateRandomNumber(0, 1000)); // Students ID number generates in range: [0;1000]
+        student.setGender(random.nextBoolean()); // Generating students gender
+        student.setMark((byte) (generateRandomNumber(0, 100))); // Mark generates in range: [0;100]
+        student.setStudyYear((byte) (generateRandomNumber(1, 6))); // Study year generates in range: [1 ; 6]
 
         // Generating random country
         String[] countries={"Ukraine", "Germany", "England", "Russia", "China", "Netherlands"};
-        student.setCountry(countries[(generateRandomNumber(0, 5))]);
+        student.setCountry(countries[(generateRandomNumber(0, countries.length - 1))]);
 
         return student;
     }
 
     private static int generateRandomNumber(int min, int max) {
+        // Generates numbers in range: [min, max]
         return random.nextInt((max - min) + 1) + min;
     }
 
