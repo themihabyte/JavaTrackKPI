1f593f2de91adaaa2859fd627ad8a66b8d5de204
themihabyte <mihabayt@gmail.com>
2020-02-26 20:24:28 +0200

    Improved validation system Added comments

--- src/m/novikov/io/github/themihabyte/view/Request.java
+++ src/m/novikov/io/github/themihabyte/view/Request.java
@@ -3,12 +3,20 @@ package m.novikov.io.github.themihabyte.view;
 import java.util.Scanner;
 
 public class Request {
-    Scanner scanner;
+    Scanner scanner; // For getting keys from keyboard
 
+    // Reads key written in console
     int getMenuKey() {
-        int key=scanner.nextInt();
-        if (Validator.validateMenuKey(key)) return key;
-        return -1;
+        int key;
+        try {
+            key=scanner.nextInt(); // Read key from console
+        } catch (Exception e){
+            System.out.println("Input another key");
+            scanner = new Scanner(System.in);
+            return -1;
+        }
+        if (Validator.validateMenuKey(key)) return key; // Check is the key corresponds to the menu items
+        return -1; // Error code (key isn`t corresponds to menu items)
     }
 
     Request() {
