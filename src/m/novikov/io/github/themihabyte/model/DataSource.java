ce65f06a932bbd48b045d80cf48a33ac8b20a706
themihabyte <mihabayt@gmail.com>
2020-03-03 19:35:32 +0200

    Deleted comments

--- src/m/novikov/io/github/themihabyte/model/DataSource.java
+++ src/m/novikov/io/github/themihabyte/model/DataSource.java
@@ -8,15 +8,14 @@ public class DataSource {
     static Student generateStudent() {
         Student student=new Student();
 
-        student.setName(generateString(generateRandomNumber(4, 11))); // length of name generates in range: [4;11]
-        student.setSurname(generateString(generateRandomNumber(5, 15))); // Length of surname generates in range [5;15]
-        student.setLastName(generateString(generateRandomNumber(5, 15))); // Length of last name generates in range [5;15]
-        student.setStudentID(generateRandomNumber(0, 1000)); // Students ID number generates in range: [0;1000]
-        student.setGender(random.nextBoolean()); // Generating students gender
-        student.setMark((byte) (generateRandomNumber(0, 100))); // Mark generates in range: [0;100]
-        student.setStudyYear((byte) (generateRandomNumber(1, 6))); // Study year generates in range: [1 ; 6]
+        student.setName(generateString(generateRandomNumber(4, 11)));
+        student.setSurname(generateString(generateRandomNumber(5, 15)));
+        student.setLastName(generateString(generateRandomNumber(5, 15)));
+        student.setStudentID(generateRandomNumber(0, 1000));
+        student.setGender(random.nextBoolean());
+        student.setMark((byte) (generateRandomNumber(0, 100)));
+        student.setStudyYear((byte) (generateRandomNumber(1, 6)));
 
-        // Generating random country
         String[] countries={"Ukraine", "Germany", "England", "Russia", "China", "Netherlands"};
         student.setCountry(countries[(generateRandomNumber(0, countries.length - 1))]);
 
@@ -24,11 +23,9 @@ public class DataSource {
     }
 
     private static int generateRandomNumber(int min, int max) {
-        // Generates numbers in range: [min, max]
         return random.nextInt((max - min) + 1) + min;
     }
 
-    // Generates random string with given length
     private static String generateString(int length) {
         StringBuilder str=new StringBuilder();
         for (int i=0; i < length; i++) {
