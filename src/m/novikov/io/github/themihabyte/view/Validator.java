e6b518d88bc4323fea8b399ba6adfd7609692c64
themihabyte <mihabayt@gmail.com>
2020-04-05 16:04:26 +0300

    regexp to validate

--- src/m/novikov/io/github/themihabyte/view/Validator.java
+++ src/m/novikov/io/github/themihabyte/view/Validator.java
@@ -1,11 +1,7 @@
 package m.novikov.io.github.themihabyte.view;
 
-public class Validator {
-    static boolean validateMenuKey(int key) {
-        if (key > 4 || key < 1) {
-            System.out.println("Input Another key");
-            return false;
-        }
-        return true;
+class Validator {
+    static boolean validateMenuKey(String key) {
+        return key.matches("[1-4]");
     }
 }
