4cf8b548ffb4b2a4c07d22f27064ff135502c3d7
themihabyte <mihabayt@gmail.com>
2020-04-05 16:03:39 +0300

    return array with copyOf method

--- src/m/novikov/io/github/themihabyte/model/Service.java
+++ src/m/novikov/io/github/themihabyte/model/Service.java
@@ -1,5 +1,7 @@
 package m.novikov.io.github.themihabyte.model;
 
+import java.util.Arrays;
+
 public class Service {
     private Student[] students;
 
@@ -7,10 +9,11 @@ public class Service {
         return students;
     }
 
-    public void setStudents(int n) {
-        students=new Student[n];
+    public void setStudents() {
+        int numberOfStudents = 10;
+        students=new Student[numberOfStudents];
 
-        for (int i=0; i < n; i++) {
+        for (int i=0; i < numberOfStudents; i++) {
             students[i]=DataSource.generateStudent();
         }
     }
@@ -18,42 +21,30 @@ public class Service {
     public Student[] getExcellentSecondYearStudents(){
         Student[] filter = new Student[students.length];
 
-        int i = 0;
+        int iterator = 0;
         for (Student student:
              students) {
             if ((student.getMark() > 95) && (student.getStudyYear() == 2)){
-                filter[i] = student;
-                i++;
+                filter[iterator] = student;
+                iterator++;
             }
         }
 
-        if (i != 0){
-            Student[] list = new Student[i];
-            System.arraycopy(filter, 0, list, 0, i);
-            return list;
-        } else {
-            return null;
-        }
+        return Arrays.copyOf(filter, iterator);
     }
 
     public Student[] getForeignWellStudyingStudents(){
         Student[] filter = new Student[students.length];
 
-        int i = 0;
+        int iterator = 0;
         for (Student student:
                 students) {
             if ((student.getMark() > 85) && (!student.getCountry().equals("Ukraine"))){
-                filter[i] = student;
-                i++;
+                filter[iterator] = student;
+                iterator++;
             }
         }
 
-        if (i != 0){
-            Student[] list = new Student[i];
-            System.arraycopy(filter, 0, list, 0, i);
-            return list;
-        } else {
-            return null;
-        }
+        return Arrays.copyOf(filter, iterator);
     }
 }
