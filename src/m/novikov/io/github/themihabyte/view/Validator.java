44bd2227145809cbd8f8060ff513a5d06a122150
themihabyte <mihabayt@gmail.com>
2020-04-05 17:40:49 +0300

    Added exception

--- src/m/novikov/io/github/themihabyte/view/Validator.java
+++ src/m/novikov/io/github/themihabyte/view/Validator.java
@@ -1,7 +1,11 @@
 package m.novikov.io.github.themihabyte.view;
 
+import m.novikov.io.github.themihabyte.exceptions.InvalidMenuItemException;
+
 class Validator {
-    static boolean validateMenuKey(String key) {
-        return key.matches("[1-4]");
+    static void validateMenuKey(String key) throws InvalidMenuItemException {
+        if (!key.matches("[1-4]")){
+            throw new InvalidMenuItemException();
+        }
     }
 }
