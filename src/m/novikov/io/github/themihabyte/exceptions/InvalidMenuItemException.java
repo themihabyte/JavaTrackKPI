230fdf199c1d0bbbb8954e17a3537be19a7de474
themihabyte <mihabayt@gmail.com>
2020-04-09 10:01:00 +0300

    Created constructor

--- src/m/novikov/io/github/themihabyte/exceptions/InvalidMenuItemException.java
+++ src/m/novikov/io/github/themihabyte/exceptions/InvalidMenuItemException.java
@@ -3,4 +3,7 @@ package m.novikov.io.github.themihabyte.exceptions;
 import java.io.IOException;
 
 public class InvalidMenuItemException extends IOException {
+    public InvalidMenuItemException(String message) {
+        super(message);
+    }
 }
