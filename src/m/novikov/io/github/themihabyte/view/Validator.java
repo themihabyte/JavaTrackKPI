c8b276c1b5cd8b67a7987616294cce1eb1d02fd9
themihabyte <mihabayt@gmail.com>
2020-04-09 10:01:38 +0300

    Message when throwing exception

--- src/m/novikov/io/github/themihabyte/view/Validator.java
+++ src/m/novikov/io/github/themihabyte/view/Validator.java
@@ -5,7 +5,7 @@ import m.novikov.io.github.themihabyte.exceptions.InvalidMenuItemException;
 class Validator {
     static void validateMenuKey(String key) throws InvalidMenuItemException {
         if (!key.matches("[1-4]")){
-            throw new InvalidMenuItemException();
+            throw new InvalidMenuItemException("Menu item " + key + " does not exist");
         }
     }
 }
