2fe2cdf4d94cb23c8600be3cf5b1d8704ee8667d
themihabyte <mihabayt@gmail.com>
2020-04-09 10:00:42 +0300

    Remove

--- src/m/novikov/io/github/themihabyte/exceptions/EmptyStudentsListException.java
+++
@@ -1,8 +0,0 @@
-package m.novikov.io.github.themihabyte.exceptions;
-
-public class EmptyStudentsListException extends Exception{
-    @Override
-    public String getMessage() {
-        return "There are not any students like that";
-    }
-}
