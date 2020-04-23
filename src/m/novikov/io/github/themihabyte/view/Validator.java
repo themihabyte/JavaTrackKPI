438e3370c34c6eaa3fcc934700afa33d40457a28
themihabyte <mihabayt@gmail.com>
2020-04-23 15:32:56 +0300

    Yes/No validation Filepath validation

--- src/m/novikov/io/github/themihabyte/view/Validator.java
+++ src/m/novikov/io/github/themihabyte/view/Validator.java
@@ -4,8 +4,19 @@ import m.novikov.io.github.themihabyte.exceptions.InvalidMenuItemException;
 
 class Validator {
     static void validateMenuKey(String key) throws InvalidMenuItemException {
-        if (!key.matches("[1-4]")){
-            throw new InvalidMenuItemException("Menu item " + key + " does not exist");
+        if (!key.matches("[1-4]")) {
+            throw new InvalidMenuItemException("Menu item "+key+" does not exist");
         }
+    }
+
+    static boolean validateYesNoKey(String key) {
+        return key.equals("Y") || key.equals("y") ||
+                key.equals("yes") || key.equals("YES") ||
+                key.equals("N") || key.equals("n") ||
+                key.equals("no") || key.equals("NO");
+    }
+
+    static boolean validateFilepath(String filepath){
+        return filepath.matches("([a-zA-Z]:(\\\\w+)*\\\\[a-zA-Z0_9]+)?.*");
     }
 }
