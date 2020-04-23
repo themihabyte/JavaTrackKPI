f323833c9da6b3462da297fd458237098834dbff
themihabyte <mihabayt@gmail.com>
2020-04-23 15:31:06 +0300

    WorkWithFile field Data source is file Write data to text file method

--- src/m/novikov/io/github/themihabyte/model/Service.java
+++ src/m/novikov/io/github/themihabyte/model/Service.java
@@ -4,47 +4,54 @@ import java.util.Arrays;
 
 public class Service {
     private Student[] students;
+    private final WorkWithFile workWithFile;
 
     public Student[] getStudents() {
         return students;
     }
 
+    public Service() {
+        workWithFile=new WorkWithFile();
+    }
+
     public void setStudents() {
-        int numberOfStudents = 10;
-        students=new Student[numberOfStudents];
-
-        for (int i=0; i < numberOfStudents; i++) {
-            students[i]=DataSource.generateStudent();
-        }
+        int numberOfStudents=10;
+        students=workWithFile.readDataFromTextFile(
+                "C:\\Users\\Admin\\Desktop\\JavaTrackKPI\\files\\DataSource.txt",
+                numberOfStudents);
     }
 
-    public Student[] getExcellentSecondYearStudents(){
-        Student[] filter = new Student[students.length];
+    public Student[] getExcellentSecondYearStudents() {
+        Student[] filter=new Student[students.length];
 
-        int iterator = 0;
-        for (Student student:
-             students) {
-            if ((student.getMark() > 95) && (student.getStudyYear() == 2)){
-                filter[iterator] = student;
-                iterator++;
-            }
-        }
-
-        return Arrays.copyOf(filter, iterator);
-    }
-
-    public Student[] getForeignWellStudyingStudents(){
-        Student[] filter = new Student[students.length];
-
-        int iterator = 0;
-        for (Student student:
+        int iterator=0;
+        for (Student student :
                 students) {
-            if ((student.getMark() > 85) && (!student.getCountry().equals("Ukraine"))){
-                filter[iterator] = student;
+            if ((student.getMark() > 95) && (student.getStudyYear() == 2)) {
+                filter[iterator]=student;
                 iterator++;
             }
         }
 
         return Arrays.copyOf(filter, iterator);
+    }
+
+    public Student[] getForeignWellStudyingStudents() {
+        Student[] filter=new Student[students.length];
+
+        int iterator=0;
+        for (Student student :
+                students) {
+            if ((student.getMark() > 85) && (!student.getCountry().equals("Ukraine"))) {
+                filter[iterator]=student;
+                iterator++;
+            }
+        }
+
+        return Arrays.copyOf(filter, iterator);
+    }
+
+    public void writeStudentsToFile(Student[] students, String filePath) {
+        workWithFile.writeDataToTextFile(students, filePath);
     }
 }
